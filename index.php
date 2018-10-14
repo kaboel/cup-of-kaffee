901a5d5a6601135704eb983069b9fd1f3e819521
kaboel <faiq.kaboel@gmail.com>
2018-10-15 04:10:59 +0700

    commit

--- index.php
+++ index.php
@@ -18,18 +18,56 @@
 <link rel="stylesheet" href="css/all.css">
 <link rel="stylesheet" href="css/style.css">
 <body>
-    <div class="container">
-        
+    <div class="main-head">
+        <div class="container">
+            <ul class="nav justify-content-end">
+                <li class="nav-item">
+                    <span class="nav-link">
+                        <i class="fas fa-info"></i>
+                        <span id="info">Yes ! We're open.</span>
+                    </span>
+                </li>
+                <li class="nav-item hyper-nav">
+                    <span class="nav-link">
+                        <i class="fas fa-shopping-cart"></i>
+                        <span id="bill">0</span>
+                    </span>
+                </li>
+            </ul>
+        </div>
     </div>
     <div class="container">
-        <div class="row">
-            <div class="col-sm-2">
-                <img src="images/kaffee-main.png" alt="" class="img-fluid">
-            </div>
-            <div class="col">
-            
-            </div>
+        <div class="side wrap">
+
         </div>
+        <div class="content wrap">
+            <!-- REMOTE -->
+        </div>
+    </div>
+    <div class="main-foot">
+        <ul class="nav justify-content-center">
+            <li class="nav-item hyper-nav">
+                <span class="nav-link">
+                    <i class="fab fa-facebook-f"></i>
+                </span>
+            </li>
+            <li class="nav-item hyper-nav">
+                <span class="nav-link">
+                    <i class="fab fa-instagram"></i>
+                </span>
+            </li>
+            <li class="nav-item hyper-nav">
+                <span class="nav-link">
+                    <i class="fab fa-twitter"></i>
+                </span>
+            </li>
+            <li class="nav-item hyper-nav">
+                <span class="nav-link">
+                    <i class="fab fa-google-plus-g"></i>
+                </span>
+            </li>
+        </ul>
+        <span class="copyright">&copy; 2018 <a href="#">.cupofkaffee.</a> | All Rights Reserved.</span>
     </div>
 </body>
 <script src="js/jquery-min.js"></script>
