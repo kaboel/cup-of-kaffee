7ce60d065f769ed3b44281f043becd86831a41e4
kaboel <faiq.kaboel@gmail.com>
2018-10-14 15:57:03 +0700

    commit

--- index.php
+++ index.php
@@ -11,13 +11,26 @@
     <meta name="author" content="faiq.kaboel@gmail.com">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>.cupofkaffee.</title>
 </head>
+<link rel="icon" href="images/kaffee-icon.png">
 <link rel="stylesheet" href="css/bootstrap.css">
 <link rel="stylesheet" href="css/all.css">
 <link rel="stylesheet" href="css/style.css">
 <body>
-    
+    <div class="container">
+        
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-2">
+                <img src="images/kaffee-main.png" alt="" class="img-fluid">
+            </div>
+            <div class="col">
+            
+            </div>
+        </div>
+    </div>
 </body>
 <script src="js/jquery-min.js"></script>
 <script src="js/bootstrap.min.js"></script>
